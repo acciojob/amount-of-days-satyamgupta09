fac4e2f4a872c4e8cfc6b6c277f3ed92db392793
satyamgupta09 <138917027+satyamgupta09@users.noreply.github.com>
2024-12-20 01:54:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function daysOfAYear(val) {
+	if((val%4===0 && val%100!==0) || val%400===0) return "366";
+	else return "365";
+}
+
+let value = promt("Enter the Year");
+alert(daysOfAYear(value));
